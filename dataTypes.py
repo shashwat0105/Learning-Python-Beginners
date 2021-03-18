918d54b6a5a575c474c07c860f368bf6a5849a7f
shashwat0105 <imt_2019093@iiitm,ac.in>
2021-03-19 00:59:39 +0530

    Added methods

---
+++ dataTypes.py
@@ -0,0 +1,17 @@
+# In python
+# string = a b c water anything
+# But in other like C++
+# char = a b c & string = water
+
+# All three below are string in python(only condition that they should be in quotes)
+x = '1'
+y = 'a'
+z = 'water'
+
+###### METHODS IN PYTHON 
+print(z.title()) # make first letter capital
+print(z.upper()) # will convert to uppercase
+print(dir("string"))  # will show all the methods of string
+
+# INDEXING & SLICING
+
