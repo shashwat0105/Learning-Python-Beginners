f4a0a1b0f8700b68c9a5279c12fa8615ef4f8024
shashwat0105 <imt_2019093@iiitm.ac.in>
2021-04-01 00:30:19 +0530

    added get close matches

--- project2 dictionary.py
+++ project2 dictionary.py
@@ -1,6 +1,7 @@
 # When we want some data we import files it may be json, csv, txt file
 # Download dictionary json file from internet (It's a large file)
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
@@ -13,9 +14,16 @@ def translate(word):
     elif word.title() in data:     # for Smog
         return data[word.title]
     elif word.upper() in data:
-        return data[word.upper]    # for USA 
-    
-    
+        return data[word.upper]    # for USA
+    elif len(get_close_matches(word, data.keys())) > 0:
+        print("Did you mean %s instead" %get_close_matches(word, data.keys())[0])  # %s is string formatting
+        decide = input("Press y for yes & n for no")
+        if decide == "y":
+            return data[get_close_matches(word, data.keys())[0]]
+        elif decide == "n":
+            return print("Word doesn't exist in this dictionary")
+        else:
+            return print("Enter just y or n")
 
     else:
         print("Word doesn't exist in this dictionary")
@@ -28,5 +36,4 @@ if type(output) == list:     #interface
         print(item)
 else:
     print(ouput)
-
 
